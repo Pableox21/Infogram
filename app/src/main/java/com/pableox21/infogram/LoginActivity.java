94d4e08745107083d0137d7130156edfbbfa86dd
pableox21 <leonax234@gmail.com>
2025-09-17 20:25:22 -0400

    15/09/25

--- app/src/main/java/com/pableox21/infogram/LoginActivity.java
+++ app/src/main/java/com/pableox21/infogram/LoginActivity.java
@@ -1,20 +1,31 @@
 package com.pableox21.infogram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.pableox21.infogram.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        androidx.core.view.WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
         setContentView(R.layout.activity_login);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+        }
     }
 
     public void goCreateAccount(View view){
-        System.out.println("--------------> go to create account");
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 
 }
